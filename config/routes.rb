bb24005b9c3e422a38fdd7bb0a3b16a720bee32b
edubew <edubew98@gmail.com>
2026-03-17 07:36:58 +0300

    add devise auth routes for login, logout and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # Resources routes
+      devise_for :users,
+      path: '',
+      path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'register'
+      },
+      controllers: {
+        sessions: 'api/v1/sessions',
+        registrations: 'api/v1/registrations'
+      }
     end
   end
 end
