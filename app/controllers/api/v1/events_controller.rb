e67c63154018788b8f79f016f14f8f458299332c
edubew <edubew98@gmail.com>
2026-03-25 07:02:49 +0300

    Prevent editing or deleting project-linked events from calendar

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -14,86 +14,96 @@ module Api
 
         render json: {
           success: true,
-          data: @events.map{|e| event_payload(e)}
+          data: @events.map { |e| event_payload(e) }
         }
       end
 
-    def show
-      render json: {success:true, data:event_payload(@event)}
-    end
+      def show
+        render json: { success: true, data: event_payload(@event) }
+      end
 
-    def create
-      @event = current_user.events.build(event_params)
-      if @event.save
-        render json: {
-          success: true,
-          data: event_payload(@event)
-        }, status: :created
-      else
+      def create
+        @event = current_user.events.build(
+          event_params.merge(source: 'manual')
+        )
+        if @event.save
+          render json: {
+            success: true,
+            data: event_payload(@event)
+          }, status: :created
+        else
+          render json: {
+            success: false,
+            errors: @event.errors.full_messages
+          }, status: :unprocessable_content
+        end
+      end
+
+      def update
+        # Prevent editing project-linked events from calendar
+        if @event.source == 'project'
+          return render json: {
+            success: false,
+            error: 'Project deadlines can only be edited from the project page'
+          }, status: :forbidden
+        end
+
+        if @event.update(event_params)
+          render json: { success: true, data: event_payload(@event) }
+        else
+          render json: {
+            success: false,
+            errors: @event.errors.full_messages
+          }, status: :unprocessable_content
+        end
+      end
+
+      def destroy
+        if @event.source == 'project'
+          return render json: {
+            success: false,
+            error: 'Project deadlines can only be removed by deleting the project'
+          }, status: :forbidden
+        end
+
+        @event.destroy
+        render json: { success: true, message: 'Event deleted' }
+      end
+
+      private
+
+      def set_event
+        @event = current_user.events.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
         render json: {
           success: false,
-          errors: @event.errors.full_messages
-        }, status: :unprocessable_content
+          error: 'Event not found'
+        }, status: :not_found
+      end
+
+      def event_params
+        params.require(:event).permit(
+          :title, :description, :start_time,
+          :end_time, :all_day, :location, :event_type
+        )
+      end
+
+      def event_payload(event)
+        {
+          id: event.id,
+          title: event.title,
+          description: event.description,
+          start: event.start_time,
+          end: event.end_time,
+          allDay: event.all_day,
+          location: event.location,
+          event_type: event.event_type,
+          color: event.color,
+          source: event.source,
+          source_id: event.source_id,
+          user_id: event.user_id
+        }
       end
     end
-
-    def update
-      if @event.update(event_params)
-        render json: {success: true, data: event_payload(@event)}
-      else
-        render json: {
-          success: false,
-          errors: @event.errors.full_messages
-        }, status: :unprocessable_content
-      end
-    end
-
-    def destroy
-      @event.destroy
-      render json: {
-        success: true,
-        message: 'Event deleted'
-      }
-    end
-
-    private
-
-    def set_event
-      @event = current_user.events.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: {
-        success: false,
-        error: 'Event not found'
-      }, status: :not_found
-    end
-
-    def event_params
-      params.require(:event).permit(
-        :title,
-        :description,
-        :start_time,
-        :end_time,
-        :all_day,
-        :location,
-        :event_type
-      )
-    end
-
-    def event_payload(event)
-      {
-        id: event.id,
-        title: event.title,
-        description: event.description,
-        start: event.start_time,
-        end: event.end_time,
-        allDay: event.all_day,
-        location: event.location,
-        event_type: event.event_type,
-        color: event.color,
-        user_id: event.user_id
-      }
-    end
-    
-  end
   end
 end
