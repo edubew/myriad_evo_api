0e75b9af3f3da0cc476fc74a1647b131327c2dbd
edubew <edubew98@gmail.com>
2026-04-01 01:23:29 +0300

    fix jwt error

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,43 +1,45 @@
-module Api
-  module V1
-    class SessionsController < Devise::SessionsController
-      respond_to :json
+class Api::V1::SessionsController < Devise::SessionsController
+  respond_to :json
 
-      def create
-        login_params = params[:user] || params.dig(:session, :user)
-        email = login_params[:email]
-        password = login_params[:password]
-        user = User.find_by(email: email)
+  def create
+    login_params = params.require(:user).permit(:email, :password)
+    # login_params = params[:user] || params.dig(:session, :user)
+    # email = login_params[:email]
+    # password = login_params[:password]
+    # user = User.find_by(email: email)
 
-          if user&.valid_password?(password)
-            token, payload = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
+    user = User.find_by(email: login_params[:email])
 
-            render json: {
-            success: true,
-            message: "Logged in successfully",
-            token: token,
-            user: {
-              id: user.id,
-              email: user.email,
-              first_name: user.first_name,
-              last_name: user.last_name,
-              role: user.role,
-              full_name: user.full_name,
-              avatar: user.avatar
-            }
-          }, status: :ok
-          
-          else
-            render json: {
-              success: false,
-              errors: ["Invalid email or password"]
-            }, status: :unauthorized
-          end
-        end
+      # if user&.valid_password?(password)
+      if user&.valid_password?(login_params[:password])
+        sign_in(user)
+        # token, payload = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
+        token = request.env['warden-jwt_auth.token']
 
-      def destroy
-        render json: { success: true, message: "Logged out successfully" }, status: :ok
+        render json: {
+        success: true,
+        message: "Logged in successfully",
+        token: token,
+        user: {
+          id: user.id,
+          email: user.email,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          role: user.role,
+          full_name: user.full_name,
+          avatar: user.avatar
+        }
+      }, status: :ok
+      
+      else
+        render json: {
+          success: false,
+          errors: ["Invalid email or password"]
+        }, status: :unauthorized
       end
     end
+
+  def destroy
+    render json: { success: true, message: "Logged out successfully" }, status: :ok
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
         #  Roles
         ROLES = %w[admin member].freeze
